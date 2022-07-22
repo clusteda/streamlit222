698b0a1092ba8b4d8c7a0c9b53c5b3a5427c7145
Vladislav Kruglikov <lildatascientist@gmail.com>
2022-07-22 11:53:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,87 +3,107 @@ import jsonlines
 import streamlit as st
 
 
-from typing import *
+from typing import Optional, Tuple, List, Set
+
 from annotated_text import annotated_text
 
 
-datasets = glob.glob("data/processed/*")
+def _iob1_start_of_chunk(
+    prev_bio_tag: Optional[str],
+    prev_conll_tag: Optional[str],
+    curr_bio_tag: str,
+    curr_conll_tag: str,
+) -> bool:
+    if curr_bio_tag == "B":
+        return True
+    if curr_bio_tag == "I" and prev_bio_tag == "O":
+        return True
+    if curr_bio_tag != "O" and prev_conll_tag != curr_conll_tag:
+        return True
+    return False
 
-option = st.selectbox('Choose dataset to overview', datasets)
 
-st.markdown('------', unsafe_allow_html=False)
+def iob1_tags_to_spans(
+    tag_sequence: List[str], classes_to_ignore: List[str] = None
+) -> List[str]:
+    classes_to_ignore = classes_to_ignore or []
+    spans: Set[Tuple[str, Tuple[int, int]]] = set()
+    span_start = 0
+    span_end = 0
+    active_conll_tag = None
+    prev_bio_tag = None
+    prev_conll_tag = None
+    for index, string_tag in enumerate(tag_sequence):
+        curr_bio_tag = string_tag[0]
+        curr_conll_tag = string_tag[2:]
 
-MAX_LINES = 50
-
-
-@st.cache
-def get_contigous(tokens: List, active_elements: List):
-    """
-    Given list of elements, find all contigous 
-    parts of active_element
-    1 0 0 1 1 1 -> (0, 0), (3, 5)
-    """
-    parts = []
-    start = None
-    for i, (token, tag) in enumerate(tokens):
-        if tag in active_elements:
-            if start == None:
-                start = i
-            else:
-                # To check for contigous tokens that are in active_elements
-                # for example if active_elements = [1, 2]
-                # then we need to check for 1 1 2 1
-                # and do not select 0-4
-                # but 0-2 3-3 4-4
-                if tokens[start][1] != tag:
-                    parts.append([start, i - 1])
-                    start = None
+        if curr_bio_tag not in ["B", "I", "O"]:
+            raise ValueError("Invalid tag sequence")
+        if curr_bio_tag == "O" or curr_conll_tag in classes_to_ignore:
+            # The span has ended.
+            if active_conll_tag is not None:
+                spans.add((active_conll_tag, (span_start, span_end)))
+            active_conll_tag = None
+        elif _iob1_start_of_chunk(
+            prev_bio_tag, prev_conll_tag, curr_bio_tag, curr_conll_tag
+        ):
+            # We are entering a new span; reset indices
+            # and active tag to new span.
+            if active_conll_tag is not None:
+                spans.add((active_conll_tag, (span_start, span_end)))
+            active_conll_tag = curr_conll_tag
+            span_start = index
+            span_end = index
         else:
-            if start != None:
-                parts.append([start, i - 1])
-                start = None
-    if start != None:
-        parts.append([start, i])
-    return parts
+            # bio_tag == "I" and curr_conll_tag == active_conll_tag
+            # We're continuing a span.
+            span_end += 1
+
+        prev_bio_tag = string_tag[0]
+        prev_conll_tag = string_tag[2:]
+    # Last token might have been a part of a valid span.
+    if active_conll_tag is not None:
+        spans.add((active_conll_tag, (span_start, span_end)))
+    return list(spans)
 
 
-with jsonlines.open(option, 'r') as file:
+datasets = glob.glob("data/test-002-predictions.jsonl", recursive=True)
+
+option = st.selectbox("Choose dataset to overview", datasets)
+
+st.markdown("------", unsafe_allow_html=False)
+
+MAX_LINES = 400
+
+color_mapping = {
+    "P": "#ccd5ae",
+    "N": "#d4a373",
+    "O": "#edede9",
+}
+
+with jsonlines.open(option, "r") as file:
     for i, line in enumerate(file):
+        if i < 10:
+            continue
         if i > MAX_LINES:
             break
-        highlights = list(zip(line['tokens'], line['tags']))
-        # tuple does not support item assignment
-        # add extra sapce after each token to add line breaks
-        highlights = [[token + ' ', tag] for token, tag in highlights]
-        # Unite B-P with I-P to Positive class and B-N with I-N to Negative class
-        for i, (token, tag) in enumerate(highlights):
-            if tag != 'O':
-                _, _class = tag.split('-')
-                if _class == 'P':
-                    highlights[i][1] = 'Positive'
-                elif _class == 'N':
-                    highlights[i][1] = 'Negative'
+        new_tokens = []
+        new_tags = []
+        indices = iob1_tags_to_spans(line["tags"])
+        indices = sorted(indices, key=lambda x: x[1][0])
+        prev = 0
+        for tag, (start, stop) in indices:
+            if prev != start:
+                new_tokens.append(" ".join(line["initial tokens"][prev:start]))
+                new_tags.append("O")
+            new_tokens.append(" ".join(line["initial tokens"][start : stop + 1]))
+            new_tags.append(tag)
+            prev = stop + 1
+        if prev != len(line["initial tokens"]):
+            new_tokens.append(" ".join(line["initial tokens"][prev:]))
+            new_tags.append("O")
 
-        # do not highlight O tags
-        contigous = get_contigous(highlights, ['Positive', 'Negative'])
-
-        parts = []
-        if contigous:
-            prev = 0
-            for start, end in contigous:
-                end = end + 1
-                if prev != start:
-                    parts.append([''.join([token for token, _ in highlights[prev:start]]), 'O'])
-                # All tokens in contigous must have the same tag so can take any
-                parts.append([' '.join([token for token, _ in highlights[start:end]]), highlights[start][1]])
-                prev = end
-            parts.append([''.join([token for token, _ in highlights[end:]]), 'O'])
-        else:
-            parts.append([''.join([token for token, _ in highlights[:]]), 'O'])
-
-        parts = [(token, tag) if tag != 'O' else (token) for token, tag in parts]
-
-
+        highlights = list(zip(new_tokens, new_tags))
+        parts = [(token + " ", tag, color_mapping[tag]) for token, tag in highlights]
         annotated_text(*parts)
-
-        st.markdown('------', unsafe_allow_html=False)
+        st.markdown("------", unsafe_allow_html=False)
